84fb12ee2ac0b7794d3a23e6951db7a493b304d2
tnmarsha <tnmarsha@geneva.edu>
2016-10-30 12:50:31 -0400

    First Commit

---
+++ DimondDating/DimondDating/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DimondDating.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+            //return "This is my <b>default</b> action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ DimondDating/DimondDating/Controllers/QuickSearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DimondDating.Controllers
+{
+    public class QuickSearchController : Controller
+    {
+        // GET: QuickSearch
+        public ActionResult Index()
+        {
+            return View();
+            //return "this is my <b>default</b> action";
+        }
+
+        public string Welcome(string name, int numTimes =1)
+        {
+            return HttpUtility.HtmlEncode("Hello" + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
